4fcb3484a909052f056deb8c71a5cd9c8c3c9181
Peter Shao <shaoruitai@gmail.com>
2025-10-17 18:59:47 -0400

    .

--- src/scripts/ort_session.js
+++ src/scripts/ort_session.js
@@ -1,7 +1,7 @@
 import * as ort from 'onnxruntime-web';
 ort.env.wasm.wasmPaths = new URL('ort/', window.location.origin).toString();
+ort.env.logLevel = 'error';
 console.log("Loaded ORT")
-//TODO
-const session = await ort.InferenceSession.create('/bible-rnn/model.onnx');
-//const session = "foo"
+const session = await ort.InferenceSession.create('/bible-rnn/model.onnx', {executionProviders: ['wasm']});
+console.log("Started ORT Session")
 export { session }
