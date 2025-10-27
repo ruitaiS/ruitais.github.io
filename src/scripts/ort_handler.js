2b2b69ae64feb8b5ae39cbab51361ec30a0d4571
Peter Shao <shaoruitai@gmail.com>
2025-10-26 22:14:53 -0400

    .

--- src/scripts/ort_handler.js
+++ src/scripts/ort_handler.js
@@ -1,8 +1,14 @@
 import * as ort from 'onnxruntime-web';
 
 export async function initialize_ort () {
-	ort.env.wasm.wasmPaths = new URL('ort/', window.location.origin).toString();
-	ort.env.logLevel = 'error';
-	const session = await ort.InferenceSession.create('/bible-rnn/model.onnx', {executionProviders: ['wasm']});
-	return { ort, session }
+	try {
+		ort.env.wasm.wasmPaths = new URL('ort/', window.location.origin).toString();
+		ort.env.logLevel = 'error';
+		const session = await ort.InferenceSession.create('/bible-rnn/model.onnx', {executionProviders: ['wasm']});
+		return { ort, session }
+
+	} catch (err) {
+		console.error("Failed to initialize ONNX Runtime:", err);
+		throw new Error("Model Initialization Error");
+	}
 }
